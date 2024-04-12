630f3de7080fe68c5a3b65e79f7b003fcaa56786
Hassan <muhammadhassanchannel786@gmail.com>
2024-04-13 00:57:19 +0500

    feat: tabs

--- app/editor/page.tsx
+++ app/editor/page.tsx
@@ -1,4 +1,5 @@
 import EditorNavigation from './_components/editor-navigation'
+import EditorSidebar from './_components/editor-sidebar'
 
 type Props = {}
 
@@ -6,6 +7,8 @@ const EditorPage = (props: Props) => {
   return (
     <div className='fixed top-0 bottom-0 left-0 right-0 z-[20] bg-background overflow-hidden'>
       <EditorNavigation />
+
+      <EditorSidebar />
     </div>
   )
 }
